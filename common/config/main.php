06db61b341a80d62f30eaae658870cfb3e2fa42e
Nasibullin Rafael <nsbln.rafael@gmail.com>
2020-09-09 13:16:54 +0600

    настроил подключение к бд

--- common/config/main.php
+++ common/config/main.php
@@ -1,13 +1,20 @@
 <?php
 return [
-    'aliases' => [
-        '@bower' => '@vendor/bower-asset',
-        '@npm'   => '@vendor/npm-asset',
-    ],
-    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
-    'components' => [
-        'cache' => [
-            'class' => 'yii\caching\FileCache',
-        ],
-    ],
+	'aliases' => [
+		'@bower' => '@vendor/bower-asset',
+		'@npm'   => '@vendor/npm-asset',
+	],
+	'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+	'components' => [
+		'cache' => [
+			'class' => 'yii\caching\FileCache',
+		],
+		'db' => [
+			'class' => 'yii\db\Connection',
+			'dsn' => 'pgsql:host=localhost;dbname=yii_plots',
+			'username' => 'user',
+			'password' => 'qweqwe',
+			'charset' => 'utf8',
+		],
+	],
 ];
